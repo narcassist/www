8ff86d13ec80aa3fa0bfd6338befa545cd7b7975
Scott Hodgson <Scott-D-Hodgson@users.noreply.github.com>
2020-08-09 10:51:03 -0400

    Added no response and progress (#11)
    Co-authored-by: Scott Hodgson <5344448@gmail.com>

--- js/questions.js
+++ js/questions.js
@@ -22,9 +22,26 @@
         while (s.length < size) s = "0" + s;
         return s;
     };
+    var updateProgress = function() {
+        var yesPercentage = Math.floor((questionYesCount / questionIndexes.length) * 100);
+        var noPercentage = Math.floor((questionNoCount / questionIndexes.length) * 100);
+        var yesBar = document.getElementById("progressYes");
+        yesBar.setAttribute("aria-valuenow", yesPercentage.toString());
+        yesBar.style.width = yesPercentage.toString() + "%";
+        var noBar = document.getElementById("progressNo");
+        noBar.setAttribute("aria-valuenow", noPercentage.toString());
+        noBar.style.width = noPercentage.toString() + "%";
+    };
+    var startQuestions = function() {
+        document.getElementById("progress").classList.remove('d-none');
+        animateCSS("#progress", "fadeIn").then((message) => {
+
+        });
+        nextQuestion();
+    };
     var loadQuestionProcess = function() {
         shuffle(questionIndexes);
-        document.getElementById("buttonStartQuestions").addEventListener("click", nextQuestion);
+        document.getElementById("buttonStartQuestions").addEventListener("click", startQuestions);
         var yesButtons = document.getElementsByClassName("response-yes");
         for (var i = 0, ilen = yesButtons.length; i < ilen; i++) {
             yesButtons[i].addEventListener("click", yesAnswer);
@@ -36,25 +53,41 @@
     };
     var yesAnswer = function() {
         questionYesCount++;
+        updateProgress();
         animateCSS("#question" + pad(questionIndexes[questionIndex], 2), "fadeOut").then((message) => {
             document.getElementById("question" + pad(questionIndexes[questionIndex], 2)).classList.add('d-none');
             if (questionYesCount === 6) {
                 document.getElementById("yesResponse").classList.remove('d-none');
                 animateCSS("#yesResponse", "fadeIn").then((message) => {
 
-                });                    
+                });
             } else {
                 questionIndex++;
-                nextQuestion(); 
+                if (questionIndex >= 15) {
+                    document.getElementById("noResponse").classList.remove('d-none');
+                    animateCSS("#noResponse", "fadeIn").then((message) => {
+    
+                    });                    
+                } else {
+                    nextQuestion();
+                } 
             };           
         });
     };        
     var noAnswer = function() {
         questionNoCount++;
+        updateProgress();
         animateCSS("#question" + pad(questionIndexes[questionIndex], 2), "fadeOut").then((message) => {
             document.getElementById("question" + pad(questionIndexes[questionIndex], 2)).classList.add('d-none');
             questionIndex++;
-            nextQuestion();            
+            if (questionIndex >= 15) {
+                document.getElementById("noResponse").classList.remove('d-none');
+                animateCSS("#noResponse", "fadeIn").then((message) => {
+
+                });                    
+            } else {
+                nextQuestion();
+            }            
         });
     };        
     var nextQuestion = function() {
